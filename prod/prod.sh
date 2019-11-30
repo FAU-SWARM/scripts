869d13df587e2bf7b7ea9ce325f014835ea852f9
Carl <chris.carl@intel.com>
2019-11-30 02:04:38 -0800

    faux paux 6

--- prod/prod.sh
+++ prod/prod.sh
@@ -3,7 +3,7 @@
 current_dir=$(pwd)
 
 cd /opt/repos/swarm
-sudo cp -a website/dist/swarm-website/. /var/www/html/
-sudo nohup python api/main.py
+sudo cp -va website/dist/swarm-website/. /var/www/html/
+sudo nohup python api/main.py &
 
 cd $current_dir
