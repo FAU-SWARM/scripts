112c7d512c908ef41887f0103af3c0a91fe68fe7
Carl <chris.carl@intel.com>
2019-12-02 01:37:51 -0800

    updated project view and started the vm dummy device

--- prod/prod.sh
+++ prod/prod.sh
@@ -5,5 +5,6 @@ current_dir=$(pwd)
 cd /opt/repos/swarm
 sudo cp -va website/dist/swarm-website/. /var/www/html/
 python api/main.py &
+python iot/test/device.py -s 60 --project linux-vm --device $(hostname) --cache /var/log/iot/vm &
 
 cd $current_dir
