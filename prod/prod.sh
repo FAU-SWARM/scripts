24cd0481dd3da490aed9d6c0c9eba0ce042f46d6
Carl <chris.carl@intel.com>
2019-12-05 06:32:43 -0800

    update-prod-script

--- prod/prod.sh
+++ prod/prod.sh
@@ -5,6 +5,6 @@ current_dir=$(pwd)
 cd /opt/repos/swarm
 sudo cp -va website/dist/swarm-website/. /var/www/html/
 python api/main.py &
-python iot/test/device.py -s 60 --project linux-vm --device $(hostname) --cache /var/log/iot/vm &
+python iot/examples/computer-monitor.py -s 5 --project demo --device $(hostname)-computer-stats --cache /var/log/iot/vm &
 
 cd $current_dir
