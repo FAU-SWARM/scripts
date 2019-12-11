0372c957eb899e83530c23a962dee5a3c306532d
Chris Carl Microsoft Surface Book 2 <chrisbcarl@outlook.com>
2019-12-11 04:25:21 -0500

    added a sleep

--- prod/prod.sh
+++ prod/prod.sh
@@ -5,6 +5,7 @@ current_dir=$(pwd)
 cd /opt/repos/swarm
 sudo cp -va website/dist/swarm-website/. /var/www/html/
 python api/main.py &
+sleep 10
 python iot/examples/computer-monitor.py -s 120 --project demo --device $(hostname)-computer-stats --cache /var/log/iot/vm &
 
 cd $current_dir
